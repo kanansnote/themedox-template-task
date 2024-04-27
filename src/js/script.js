41e095736e9f2065a955c56a7ac07e586ee88ddb
kanan <kanansnote@gmail.com>
2024-04-28 00:32:19 +0400

    Some work on .topTrendingContainer and footer.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 // Slider for .hotBidsCards section using swiper.js
 
-const swiper = new Swiper('.topTrendingCards', {
+const swiper = new Swiper('.topTrendingContainer', {
     slidesPerView: '3', // Adjust this value to control how many slides are visible at a time
     loop: true,
     navigation: {
