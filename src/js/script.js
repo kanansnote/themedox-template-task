64abfd08a76e5b34c89c303601803079bd720d57
kanan <kanansnote@gmail.com>
2024-04-28 21:28:30 +0400

    Minor work on some elements.

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,30 @@
 // Slider for .hotBidsCards section using swiper.js
 
 const swiper = new Swiper('.topTrendingContainer', {
-    slidesPerView: '3', // Adjust this value to control how many slides are visible at a time
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  slidesPerView: '3', // Adjust this value to control how many slides are visible at a time
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// Back to Top Button
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    topBtn.style.display = "block";  /* Show button when scrolled down */
+  } else {
+    topBtn.style.display = "none";   /* Hide button when scrolled up */
+  }
+}
+
+topBtn.addEventListener("click", function() {
+  document.documentElement.scrollIntoView({
+    behavior: 'smooth',  // Enables smooth scrolling animation
+    block: 'start'       // Scrolls to the top of the page
   });
+});
